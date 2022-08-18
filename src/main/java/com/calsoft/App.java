d58744085ed62bfe1300eb60370104f1ac2b1ed2
root <root@ip-172-31-6-110.ap-south-1.compute.internal>
2022-08-18 09:20:24 +0000

    initial commit

---
+++ src/main/java/com/calsoft/App.java
@@ -0,0 +1,15 @@
+package com.calsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //added changes to test scm build fire 2
+        //testing webhooks3
+    }
+}
